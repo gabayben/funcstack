b12af0fda453931b505b37e38ef610271bd08e5e
bengabay <ben@netop.cloud>
2024-05-07 14:45:30 +0300

    PromptBuilder

--- libs/funcstack/funcstack/modules/builders/prompt.py
+++ libs/funcstack/funcstack/modules/builders/prompt.py
@@ -1,23 +1,32 @@
 from typing import Any, Type, override
 
-from pydantic import Field
+from funcstack.utils.serialization import create_model
+from jinja2 import Template, meta
+from pydantic import BaseModel
 
-from funcstack.containers import Effect
+from funcstack.containers import Effect, Effects
 from funcstack.mixins import PydanticMixin
 from funcstack.modules import Module
-from funcstack.typing._vars import In
 
-class PromptBuilder(PydanticMixin, Module[In, str]):
-    template: str
-    context_type: Type[In] = Field(default=Any)
+class PromptBuilder(PydanticMixin, Module[dict[str, Any], str]):
+    template_str: str
+    required_variables: list[str]
 
-    @property
+    def __init__(self, template: str, required_variables: list[str] | None = None):
+        super().__init__(template_str=template, required_variables = required_variables or [])
+        self.template = Template(template)
+        ast = self.template.environment.parse(template)
+        template_variables = meta.find_undeclared_variables(ast)
+        self.context_variables = {v: (Any, None) for v in template_variables}
+
+    def evaluate(self, data: dict[str, Any], **kwargs) -> Effect[str]:
+        def _invoke() -> str:
+            missing_variables = [var for var in self.required_variables if var not in data]
+            if missing_variables:
+                raise ValueError(f'Missing required input variables in PromptBuilder: {', '.join(missing_variables)}.')
+            return self.template.render(data)
+        return Effects.Sync(_invoke)
+
     @override
-    def InputType(self) -> Type[In]:
-        return self.context_type
-
-    def __init__(self, template: str, context_type: Type[In] = Any):
-        super().__init__(template=template, context_type=context_type)
-
-    def evaluate(self, data: In, **kwargs) -> Effect[str]:
-        pass
+    def input_schema(self) -> Type[BaseModel]:
+        return create_model(self.get_name(name='Input'), **self.context_variables)
